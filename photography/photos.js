49c0a068f4b62df8ef6231babf88c9ceaf913224
hongwei li <hongweixli@tencent.com>
2022-10-20 09:31:05 +0800

    stashed

---
+++ photography/photos.js
@@ -0,0 +1,17 @@
+
+var photos = {
+    "projects": [
+        {"name": "shanghai-ferries", "photos": 72},
+        {"name": "metro", "photos": 72},
+        {"name": "epidemic-epidemic-epidemic", "photos": 72},
+        {"name": "home", "photos": 72},
+        {"name": "城市精灵", "photos": 72},
+        {"name": "here-and-there", "photos": 72},
+        {"name": "son", "photos": 72}
+    ],
+    "travels": [
+        {"name": "japan", "photos": 41},
+        {"name": "singapore", "photos": 41},
+        {"name": "yang-zhou", "photos": 41}
+    ]
+};
