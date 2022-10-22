52f927b07228e1627599dcd13bc1588b15eceea4
Hongwei Li <lihw81@gmail.com>
2022-10-22 21:02:01 +0800

    stashed

--- photography/photos.js
+++ photography/photos.js
@@ -4,7 +4,7 @@ var photos = {
         {"name": "shanghai-ferries", "photos": 78},
         {"name": "2020-pan", "photos": 72},
         {"name": "metro", "photos": 72},
-        {"name": "epidemic-epidemic-epidemic", "photos": 72},
+        {"name": "epidemic-epidemic-epidemic", "photos": 49},
         {"name": "home", "photos": 72},
         {"name": "城市精灵", "photos": 72},
         {"name": "here-and-there", "photos": 72},
